8e88c5074f6b59940680ef9a044ec254cbf14968
salzanopm <salzanopm@gmail.com>
2022-01-21 17:24:29 +0100

    finish

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    // inizializzazione variabile paragrafo
+    $paragraph = 'Non posso dire questo perchè questo è censurato';
+
+    // parametro get per passare stringa da censurare
+    $badword = $_GET['badword'];
+
+    // sostituisco la parola parametrizzata dall'utente con gli asterischi
+    $replace_paragraph = str_replace($badword, '***', $paragraph);
+?>
+
+<!-- stampo a schermo la stringa -->
+<p><?php echo $paragraph;?></p>
+
+<!-- stampo a schermo la lunghezza della stringa -->
+<p>La lunghezza del titolo qui sopra è di <?php  echo strlen($paragraph);?> </p>
+
+<!--  ristampo a schermo stringa censurata -->
+<p><?php echo $replace_paragraph;?> </p>
+
